fe12582a4440bd790b605dfe77afe440f51c7989
jialong <smt02091395@gmail.com>
2023-10-24 16:03:25 +0800

    231024 Add week7 homework

---
+++ AI_lab/112-1/412770116_許嘉隆_20231024_課堂作業/todo_list/script.js
@@ -0,0 +1,36 @@
+let add_btn = document.querySelector('#add_btn')
+let add_input = document.querySelector('#add_input')
+let list = document.querySelector('#list');
+let data = []
+
+function render(){
+  list.innerHTML = ''
+  for(let i of data){
+    list.innerHTML += `
+    <div class="item" data-id="${i.id}">
+      <li>${i.content}</li>
+      <button type="submit" class="delete" >X</button>
+    </div>
+  `;   
+  }
+}
+
+function addItem(e){
+  if(add_input.value && (e.key == "Enter" || !e.key)){
+    data.push({id:new Date().getTime(), content: add_input.value});
+    render();
+    add_input.value = ''
+  }
+}
+
+function removeItem(e){
+  let id = e.target.closest("div").dataset.id;
+  if(e.target.getAttribute("class") == "delete"){
+    data.splice(data.findIndex(i => id == i.id),1);
+  }
+  render();
+}
+
+add_btn.addEventListener('click', addItem)
+document.addEventListener('keyup', addItem)
+list.addEventListener('click', removeItem)
