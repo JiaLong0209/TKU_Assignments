74082b3f2e63206493210a39c816d560bbeb3d69
JiaLong0209 <smt02091395@gmail.com>
2023-11-22 23:33:42 +0800

    231122 Create x, y data

--- Introduction_to_AI/LR/LinearRegression.py
+++ Introduction_to_AI/LR/LinearRegression.py
@@ -1,1 +1,14 @@
-print('hello, linear regression')
+def linear_regression(x, y):
+    print(x)
+    print(y)
+    
+    return [x*y for x,y in zip(x, y)]
+
+# Seniority
+x = [1,2,3,3.5,4,4.5,6,6,7]
+# Salary
+y = [30000,25000,35000,32000,35000,32000,35000,50000,40000] 
+
+line = linear_regression(x, y)
+
+print(line)
