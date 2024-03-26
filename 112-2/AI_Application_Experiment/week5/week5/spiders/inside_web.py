590a755bc53a685db2d444375bc72316e935f283
JiaLong0209 <smt02091395@gmail.com>
2024-03-26 15:35:36 +0800

    240326 Upload AIAE week5 scrapy

---
+++ 112-2/AI_Application_Experiment/week5/week5/spiders/inside_web.py
@@ -0,0 +1,26 @@
+import scrapy
+from datetime import datetime 
+from tqdm import tqdm
+import bs4 
+
+
+class InsideWebSpider(scrapy.Spider):
+    name = "inside_web"
+    allowed_domains = ["www.inside.com.tw"]
+    start_urls = ["https://www.inside.com.tw/"]
+    page_param = '?page='
+
+    def parse(self, response):
+        for i in range(1, 100):  
+            yield scrapy.Request(url=response.urljoin(self.page_param + str(i)), callback=self.parse_page_number)
+
+    def parse_page_number(self, response):
+        soup = bs4.BeautifulSoup(response.text, 'lxml')
+        titles = soup.find_all('h3', {'class': 'post_title'})
+
+        for title in titles:
+            print(f"\nURL: {response.url}")
+            print(f"Title: {title.text.str  ip()}")
+            yield {'title': title.text.strip()}
+        
+    
