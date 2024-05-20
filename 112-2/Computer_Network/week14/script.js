84f30e59b28d030789eb5c7f3083c72383e3e730
JiaLong0209 <smt02091395@gmail.com>
2024-05-20 16:36:20 +0800

    240520 Upload week14 practice

---
+++ 112-2/Computer_Network/week14/script.js
@@ -0,0 +1,14 @@
+let $ = (s) => document.querySelector(s);
+let $a = (s) => document.querySelectorAll(s);
+
+let p = $(`#demo1`);
+let x = "10" + 5 ;
+
+
+console.log(p)
+document.innerHTML = x;
+
+document.write("<h1> Write </h1>");
+document.write("<h1> Write </h1>");
+
+p.write("efojesif");
---
+++ 112-2/Computer_Programming/week14/find_max_in_rows.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 3
+
+void findMaxInRows(int arr[N][N]){
+	for(int i = 0; i < N; i++){
+		int max = arr[i][0];
+		for(int j = 0; j < N; j++) max = arr[i][j] > max ? arr[i][j] : max;
+		printf("The max in %d row: %d \n", i+1, max);
+	}
+}
+
+int main(){
+	int arr[3][3] = {{1,3,2},{5,4,6},{7,8,9}};
+	findMaxInRows(arr);
+
+}
+
