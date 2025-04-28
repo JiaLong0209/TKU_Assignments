b95d75f873e3f987ffe3875c853c9e4629f59505
JiaLong0209 <smt02091395@gmail.com>
2025-04-29 03:03:27 +0800

    250429 implement kruskal algorithmls

--- 113-2/Algorithm/kruskal.py
+++ 113-2/Algorithm/kruskal.py
@@ -1,19 +1,90 @@
-def kruskal():
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
 
+    def add_edge(self, start, end, w):
+        self.graph.append([start, end, w])
+
+    def find(self, parent, i):
+        if parent[i] != i:
+            parent[i] = self.find(parent, parent[i])
+        return parent[i]
+
+    def union(self, parent, rank, x, y):
+        print(f'-- union --')
+        print(f'    x: subsets[{x}] | parent = {parent[x]} | rank = {rank[x]}')
+        print(f'    y: subsets[{y}] | parent = {parent[y]} | rank = {rank[y]}')
+        if(rank[x] <  rank[y]):
+            parent[x] = y
+        elif(rank[x] > rank[y]):
+            parent[y] = x
+        else:
+            parent[y] = x
+            rank[x] += 1
+        print(f'    x: subsets[{x}] | parent = {parent[x]} | rank = {rank[x]}')
+        print(f'    y: subsets[{y}] | parent = {parent[y]} | rank = {rank[y]}')
+
+
+    def show_graph(self, graph):
+        for start, end, weight in graph:
+            print(f'    {start} <---> {end} | weight: {weight}')
+        print('')
+
+    def kruskal_MST(self):
+
+        result = []
+        parent = []
+        rank = []
+
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+            
+
+        print(f'=== original graph ===')
+        self.show_graph(self.graph)
+        
+        self.graph = sorted(self.graph, key=lambda items: items[2])
+
+        print(f'=== sorted graph ===')
+        self.show_graph(self.graph)
+
+        for index, next_edge in enumerate(self.graph):
+            if(len(result) == self.V-1):
+                break
+
+            x = next_edge[0] # start node
+            y = next_edge[1] # end node
+
+            x_root = self.find(parent, x)
+            y_root = self.find(parent, y)
+
+            if (x_root != y_root):
+                result.append(next_edge)
+                self.union(parent, rank, x_root, y_root)
+
+
+        return result
+            
 
 def main():
 
-    # 0 1 1
-    # 0 2 3
-    # 1 2 3 
-    # 1 3 6 
-    # 2 3 4 
-    # 2 4 2 
-    # 3 4 5 
+    g = Graph(7)
+    g.add_edge(0, 1, 1)
+    g.add_edge(0, 2, 3)
+    g.add_edge(1, 2, 3)
+    g.add_edge(1, 3, 6)
+    g.add_edge(2, 3, 4)
+    g.add_edge(2, 4, 2)
+    g.add_edge(3, 4, 5)
 
-
+    result = g.kruskal_MST()
+    print("\n== Result ==")
+    g.show_graph(result)
 
 main()
+
 
 
 
